1546e4650bae600cbcfd26e9a0c8dec4a4597085
Neil <neil.moncur@outlook.com>
2020-11-22 11:25:04 -0500

    Changed folder name; going to try working from Visual Studio Code

---
+++ DotNet-Solution/KneelDB/KneelDB.Core/Query.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KneelDB.Core
+{
+    public class Query
+    {
+        public int Insert(string dbName, string tableName, Dictionary<string,string> values)
+        {
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
