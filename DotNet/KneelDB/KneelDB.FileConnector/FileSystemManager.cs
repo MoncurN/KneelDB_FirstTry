1d8d5f18939d96e4520fad2e2a06a1f88e0e1c5d
Neil <neil.moncur@outlook.com>
2020-11-21 23:23:07 -0500

    Started on query stack

---
+++ DotNet/KneelDB/KneelDB.FileConnector/FileSystemManager.cs
@@ -0,0 +1,38 @@
+﻿using KneelDB.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace KneelDB.FileConnector
+{
+    public class FileSystemManager
+    {
+        public Database Read(string databaseName, string tableName)
+        {
+            if (!Directory.Exists(databaseName))
+            {
+                Directory.CreateDirectory(databaseName);
+            }
+
+            Database database = null;
+
+            if (!File.Exists(databaseName + @"\" + tableName + @".json"))
+            {
+                database = new Database();
+            }
+            else
+            {
+                var json = File.ReadAllText(databaseName + @"\" + tableName + @".json");
+                database = JsonConvert.DeserializeObject<Database>(json);
+            }
+
+            return database;
+        }
+
+        public void Write(Database database)
+        {
+
+        }
+    }
+}
